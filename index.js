20b33a95b3dbb10bf0294179c3a65d99b6e591e4
Dale Holstein <Holstein30@live.com>
2019-10-22 18:57:57 -0400

    Refactor - Routes

--- index.js
+++ index.js
@@ -13,12 +13,6 @@ passport.use(new GoogleStrategy({
     console.log({accessToken, refreshToken, profile});
 }));
 
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-}));
-
-app.get('/auth/google/callback', passport.authenticate('google'))
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
