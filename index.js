4f2c68c3614cf45685003ca24289629f03af1838
Dale Holstein <Holstein30@live.com>
2019-10-22 18:58:38 -0400

    Refactor - Services

--- index.js
+++ index.js
@@ -5,14 +5,6 @@ const keys = require('./config/keys');
 
 const app = express();
 
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
-    console.log({accessToken, refreshToken, profile});
-}));
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
---
+++ services/passport.js
@@ -0,0 +1,7 @@
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+    console.log({accessToken, refreshToken, profile});
+}));
