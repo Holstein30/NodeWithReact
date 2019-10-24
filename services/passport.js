2650b95f3e748cf0077b4da5e2bcf4e72a13aa45
Dale Holstein <Holstein30@live.com>
2019-10-24 09:24:34 -0400

    DB Setup - Cookies Enabled

--- services/passport.js
+++ services/passport.js
@@ -9,20 +9,31 @@ passport.serializeUser((user, done) => {
     done(null, user.id);
 });
 
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
-    User.findOne({ googleId: profile.id }).then((existingUser) => {
-        if(existingUser){
-            // Already have record with this id
-            done(null, existingUser);
-        } else {
-            // Add new
-            new User({ googleId: profile.id})
-                .save()
-                .then(user => done(null, user));
-        }
+passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => {
+        done(null, user);
     });
-}));
+});
+
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+        },
+        (accessToken, refreshToken, profile, done) => {
+            User.findOne({ googleId: profile.id }).then(existingUser => {
+                if (existingUser) {
+                    // Already have record with this id
+                    done(null, existingUser);
+                } else {
+                    // Add new
+                    new User({ googleId: profile.id })
+                        .save()
+                        .then(user => done(null, user));
+                }
+            });
+        }
+    )
+);
