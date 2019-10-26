62b9f67f15a914d8a9ef84e5244f2f55e11d1b7b
Dale Holstein <Holstein30@live.com>
2019-10-26 12:16:16 -0400

    Dev & Prod - Dev config

---
+++ config/dev.js
@@ -0,0 +1,9 @@
+// dev.js
+module.exports = {
+    googleClientID:
+        '958564880080-rad6nkofcmuaikedii2jndal6af6pdlg.apps.googleusercontent.com',
+    googleClientSecret: 'ZcsgQuQtM8nkwmiHpp_8qoqC',
+    mongoURI:
+        'mongodb+srv://dbUser:hjI8yJfUVmkhq5qx@emaily-frw6s.mongodb.net/test?retryWrites=true&w=majority',
+    cookieKey: 'akjsdflkjbvievmmmviekdsakdsjflkaj'
+};
