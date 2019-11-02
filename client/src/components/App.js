8fa335719163aa45a17346e9f2de5ebf984ce2b4
Dale Holstein <Holstein30@live.com>
2019-11-02 19:53:10 -0400

    Client Side - basic routing test complete

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,7 +7,18 @@ const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
-    return <div>Howdy</div>;
+    return (
+        <div>
+            <BrowserRouter>
+                <Header />
+                <div>
+                    <Route path="/" exact component={Landing} />
+                    <Route path="/surveys" exact component={Dashboard} />
+                    <Route path="/surveys/new" component={SurveyNew} />
+                </div>
+            </BrowserRouter>
+        </div>
+    );
 };
 
 export default App;
