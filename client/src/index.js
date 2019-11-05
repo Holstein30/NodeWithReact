1e71686a3dc769d0123b68d54df15cd2ca628edb
Dale Holstein <Holstein30@live.com>
2019-11-05 06:56:34 -0500

    Payments - Payments component

--- client/src/index.js
+++ client/src/index.js
@@ -16,6 +16,3 @@ ReactDOM.render(
     </Provider>,
     document.querySelector('#root')
 );
-
-console.log('STRIPE KEY = ', process.env.REACT_APP_STRIPE_KEY);
-console.log('Environment = ', process.env.NODE_ENV);
